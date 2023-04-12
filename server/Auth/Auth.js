29bd03a78a9403c50beaf22f5e29574d94542b40
+ <andrewjohnjulian@gmail.com>
2023-04-11 20:41:29 -0600

    added jswtokens

--- server/Auth/Auth.js
+++ server/Auth/Auth.js
@@ -1,5 +1,7 @@
 const User = require("../model/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JWT_SECRET;
 
 exports.register = async (req, res, next) => {
   const { username, password, email } = req.body;
@@ -10,12 +12,24 @@ exports.register = async (req, res, next) => {
         password: hash,
         email,
       })
-        .then((user) =>
-          res.status(200).json({
+        .then((user) => {
+          const maxAge = 3 * 60 * 60;
+          const token = jwt.sign(
+            { id: user._id, username, role: user.role },
+            jwtSecret,
+            {
+              expiresIn: maxAge, // 3hrs in sec
+            }
+          );
+          res.cookie("jwt", token, {
+            httpOnly: true,
+            maxAge: maxAge * 1000, // 3hrs in ms
+          });
+          res.status(201).json({
             message: "User successfully created",
-            user,
-          })
-        )
+            user: user._id,
+          });
+        })
         .catch((error) =>
           res.status(400).json({
             message: "User not successful created",
