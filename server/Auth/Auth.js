a06742f319e4b2784dafe21825444a5437771c40
+ <andrewjohnjulian@gmail.com>
2023-04-11 20:34:29 -0600

    updated login with password comparison

--- server/Auth/Auth.js
+++ server/Auth/Auth.js
@@ -33,17 +33,28 @@ exports.register = async (req, res, next) => {
 
 exports.login = async (req, res, next) => {
   const { username, password } = req.body;
+  // Check if username and password is provided
+  if (!username || !password) {
+    return res.status(400).json({
+      message: "Username or Password not present",
+    });
+  }
   try {
-    const user = await User.findOne({ username, password });
+    const user = await User.findOne({ username });
     if (!user) {
-      res.status(401).json({
+      res.status(400).json({
         message: "Login not successful",
         error: "User not found",
       });
     } else {
-      res.status(200).json({
-        message: "Login successful",
-        user,
+      // comparing given password with hashed password
+      bcrypt.compare(password, user.password).then(function (result) {
+        result
+          ? res.status(200).json({
+              message: "Login successful",
+              user,
+            })
+          : res.status(400).json({ message: "Login not succesful" });
       });
     }
   } catch (error) {
