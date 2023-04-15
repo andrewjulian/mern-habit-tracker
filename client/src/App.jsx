31088a8f43d7aa1e36849f9bb94ac786ffbd6023
+ <andrewjohnjulian@gmail.com>
2023-04-14 19:52:09 -0600

    updated routes to previous format with browserrouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,34 +2,17 @@ import { useEffect, useContext } from "react";
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import { UserContext } from "./Context/userContext";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import Profile from "./Components/Profile";
 
 function App() {
   const [user, setUser] = useContext(UserContext);
-  let users = [];
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await fetch("/api/auth/getUsers", {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        const data = await response.json();
-        users = data;
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchUser();
-  }, []);
-
-  return (
-    <div>
-      {users}
-      <Outlet />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,36 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Error from "./Components/Error";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Login from "./Components/Login";
-import Landing from "./Components/Landing";
-import SignUp from "./Components/SignUp";
-import Navbar from "./Components/Navbar";
+import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./Context/userContext";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <Error />,
-    children: [
-      { path: "/", element: <Login /> },
-      { path: "/login", element: <Login /> },
-      { path: "/signup", element: <SignUp /> },
-      {
-        path: "/home",
-        element: <Navbar />,
-        children: [{ path: "/home/landing", element: <Landing /> }],
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <UserProvider>
-      <RouterProvider router={router} />
-    </UserProvider>
+    <BrowserRouter>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
