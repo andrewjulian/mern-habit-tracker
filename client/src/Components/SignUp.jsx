480bd989385baf7e6c060e5408befd140d8b98f0
+ <andrewjohnjulian@gmail.com>
2023-04-17 22:11:50 -0600

    styling for signup page

--- client/src/Components/SignUp.jsx
+++ client/src/Components/SignUp.jsx
@@ -33,35 +33,67 @@ const SignUp = ({ setUser }) => {
   };
 
   return (
-    <div>
-      <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <input
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          name="email"
-          id="email"
-          value={email}
+    <div className="flex min-h-full items-center justify-center px-4 py-12 mt-[100px] sm:px-6 lg:px-8">
+      <div className="w-full max-w-md space-y-8">
+        <img
+          className="mx-auto h-12 w-auto"
+          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          alt="Your Company"
         />
-        <label htmlFor="password">Password</label>
-        <input
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          name="password"
-          id="password"
-          value={password}
-        />
-        <label htmlFor="username">Username</label>
-        <input
-          onChange={(e) => setUsername(e.target.value)}
-          type="text"
-          name="username"
-          id="username"
-          value={username}
-        />
-        <button type="submit">Sign Up</button>
-      </form>
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+          <div className="-space-y-px rounded-md shadow-sm">
+            <label className="sr-only" htmlFor="email">
+              Email
+            </label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Email Address"
+            />
+          </div>
+          <div>
+            <label className="sr-only" htmlFor="password">
+              Password
+            </label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Password"
+            />
+          </div>
+          <div>
+            <label className="sr-only" htmlFor="username">
+              Username
+            </label>
+            <input
+              onChange={(e) => setUsername(e.target.value)}
+              type="text"
+              name="username"
+              id="username"
+              value={username}
+              className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Create Username"
+            />
+          </div>
+          <button
+            className="group relative flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            type="submit"
+          >
+            Sign Up
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
