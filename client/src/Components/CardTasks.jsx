fa60989a424a299c76ffafe40f156e67601357e6
+ <andrewjohnjulian@gmail.com>
2023-04-21 13:23:08 -0600

    adjusted sizes and margins of task status

--- client/src/Components/CardTasks.jsx
+++ client/src/Components/CardTasks.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { BiEdit } from "react-icons/bi";
+import { BiCircle } from "react-icons/bi";
+import { MdOutlineHighlight } from "react-icons/md";
 import { AiFillCheckCircle } from "react-icons/ai";
 import { BsArrowRightCircleFill } from "react-icons/bs";
-import { BsCircleHalf } from "react-icons/bs";
-import { BiCircle } from "react-icons/bi";
-import { BsXCircle } from "react-icons/bs";
-import { MdOutlineHighlight } from "react-icons/md";
 import { IoPeopleCircleOutline } from "react-icons/io5";
+import { BsCircleHalf } from "react-icons/bs";
+import { BsXCircle } from "react-icons/bs";
 
 const CardTasks = () => {
   const [status, setStatus] = useState(0);
@@ -20,26 +20,38 @@ const CardTasks = () => {
   };
 
   const statusButton = [
-    <BiCircle onClick={handleStatus} size={22} className="mr-2" />,
-    <MdOutlineHighlight onClick={handleStatus} size={22} className="mr-2" />,
-    <AiFillCheckCircle onClick={handleStatus} size={22} className="mr-2" />,
-    <BsArrowRightCircleFill
+    <BiCircle onClick={handleStatus} size={22} className="ml-2 mr-1.5" />,
+    <MdOutlineHighlight
       onClick={handleStatus}
       size={22}
-      className="mr-2"
+      className="ml-2 mr-1.5"
     />,
-    <IoPeopleCircleOutline onClick={handleStatus} size={22} className="mr-2" />,
-    <BsCircleHalf onClick={handleStatus} size={22} className="mr-2" />,
-    <BsXCircle onClick={handleStatus} size={22} className="mr-2" />,
+    <AiFillCheckCircle
+      onClick={handleStatus}
+      size={22}
+      className="ml-2 mr-1.5"
+    />,
+    <BsArrowRightCircleFill
+      onClick={handleStatus}
+      size={18}
+      className="ml-2.5 mr-2"
+    />,
+    <IoPeopleCircleOutline
+      onClick={handleStatus}
+      size={22}
+      className="ml-2 mr-1.5"
+    />,
+    <BsCircleHalf onClick={handleStatus} size={18} className="ml-2.5 mr-2" />,
+    <BsXCircle onClick={handleStatus} size={18} className="ml-2.5 mr-2" />,
   ];
 
   const displayStatus = statusButton[status];
   return (
     <div>
-      <div className="flex align-middle justify-between my-1">
+      <div className="flex align-middle justify-between my-1 py-3 border-b border-[#7e6e45]">
         <div className="flex items-center">
           {displayStatus}
-          <h1 className="text-decoration-line">The Highlight will go here</h1>
+          <h1>The Highlight will go here</h1>
         </div>
         <BiEdit color={"#7e6e45"} size={25} />
       </div>
