13f0dcdaca38931d92d9022a94e18f440d01768c
+ <andrewjohnjulian@gmail.com>
2023-04-20 22:07:38 -0600

    item elements present, needs edit push to right

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -1,8 +1,33 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
+import { BiEdit } from "react-icons/bi";
+import { AiFillCheckCircle } from "react-icons/ai";
+import { BsArrowRightCircleFill } from "react-icons/bs";
+import { BsCircleHalf } from "react-icons/bs";
 
 const Card = () => {
   const [startDate, setStartDate] = useState(new Date());
+  const [status, setStatus] = useState(0);
+
+  const handleStatus = () => {
+    if (status === 2) {
+      setStatus(0);
+    } else {
+      setStatus(status + 1);
+    }
+  };
+
+  const statusButton = [
+    <AiFillCheckCircle onClick={handleStatus} size={22} className="mr-2" />,
+    <BsArrowRightCircleFill
+      onClick={handleStatus}
+      size={22}
+      className="mr-2"
+    />,
+    <BsCircleHalf onClick={handleStatus} size={22} className="mr-2" />,
+  ];
+
+  const displayStatus = statusButton[status];
 
   return (
     <div className="flex items-center h-screen">
@@ -18,6 +43,20 @@ const Card = () => {
                   selected={startDate}
                   onChange={(date) => setStartDate(date)}
                 />
+              </div>
+              <div className=" items-center">
+                <h3 className="text-gray-600 text-md my-2 mr-5">Highlight:</h3>
+                <div className="flex align-middle">
+                  <div className="flex items-center justify-between">
+                    {displayStatus}
+                    <h1 className="text-decoration-line">
+                      The Highlight will go here
+                    </h1>
+                  </div>
+                  <div>
+                    <BiEdit color={"blue"} size={25} />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
