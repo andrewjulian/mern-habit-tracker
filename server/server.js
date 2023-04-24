bbac446f18b4736851fefab73c6df18c28881676
+ <andrewjohnjulian@gmail.com>
2023-04-23 19:45:26 -0600

    reorganize server.js

--- server/server.js
+++ server/server.js
@@ -1,22 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-require("dotenv").config();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
-
-app.use(cors());
-app.use(express.json());
-app.use(cookieParser());
-
 const uri = process.env.ATLAS_URI;
 
 mongoose
@@ -31,6 +22,11 @@ mongoose
     console.error("Error connecting to MongoDB Atlas:", error);
   });
 
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use("/api/auth", require("./Auth/Route"));
 app.use("/api/card", require("./Cards/Route"));
 app.use("/api/task", require("./Tasks/Route"));
