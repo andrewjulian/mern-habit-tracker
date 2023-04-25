934058d2306d66932e49c4d5252cde7a8514c0aa
+ <andrewjohnjulian@gmail.com>
2023-04-25 14:42:48 -0600

    added back auth and card basics

--- server/Cards/Card.js
+++ server/Cards/Card.js
@@ -2,21 +2,6 @@ const Card = require("../model/CardModel");
 const mongoose = require("mongoose");
 const ObjectId = mongoose.Types.ObjectId;
 
-const userCards = async (req, res) => {
-  try {
-    const { userId } = req.body;
-    const cards = await Card.find({ user: ObjectId(userId) }).populate({
-      path: "cardTasks",
-      select: "status text",
-    });
-    res.status(200).json({ succes: true, cards });
-  } catch (error) {
-    res.status(400).json({ succes: false, error });
-  }
-};
-
-module.exports = { userCards };
-
 const cards = async (req, res) => {
   try {
     const cards = await Card.find().populate({
