c6ec1cf8897e88b08b34ad2a67f2b181050457c2
+ <andrewjohnjulian@gmail.com>
2023-04-26 21:37:11 -0600

    new server.js setup

--- server/server.js
+++ server/server.js
@@ -5,6 +5,13 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+const passportlocal = require("passport-local").Strategy;
+const session = require("express-session");
+
+const User = require("./User/Model");
+
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
@@ -22,11 +29,23 @@ mongoose
     console.error("Error connecting to MongoDB Atlas:", error);
   });
 
-app.use(cors());
-app.use(express.json());
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(cors({ origin: "http://127.0.0.1:5175/", credentials: true }));
+
+app.use(
+  session({
+    secret: uri,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(cookieParser(uri));
+app.use(passport.initialize());
+app.use(passport.session());
+require("./User/PassportConfig")(passport);
 
 app.use("/api/user", require("./User/Route"));
 app.use("/api/card", require("./Cards/Route"));
