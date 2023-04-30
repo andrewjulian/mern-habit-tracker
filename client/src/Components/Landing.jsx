4969e4425012e89d1f0421ac96167316ca8a331a
+ <andrewjohnjulian@gmail.com>
2023-04-29 19:43:04 -0600

    added stock landing page content

--- client/src/Components/Landing.jsx
+++ client/src/Components/Landing.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Hi</h1>
+    <div className="m-[15px]">
+      <h1 className="text-center font-mono text-3xl font-bold  text-gray-900">
+        Welcome to Digital
+      </h1>
+      <h1 className="text-center font-mono my-[4px] text-xl font-bold tracking-tight text-gray-900">
+        Your Thoughtful Planning Tool
+      </h1>
     </div>
   );
 };
