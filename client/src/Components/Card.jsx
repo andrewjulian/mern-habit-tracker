6e61a1b986794d990d59c48dae5952b87b422067
+ <andrewjohnjulian@gmail.com>
2023-05-03 21:16:54 -0600

    cards populated, but are only card ids, not full cards

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import CardTasks from "./CardTasks";
 
-const Card = () => {
+const Card = ({ card }) => {
   const [startDate, setStartDate] = useState(new Date());
 
   /* const displayTasks = tasks.map((task, index) => {
@@ -10,18 +10,15 @@ const Card = () => {
   }); */
 
   return (
-    <div className="flex items-center h-screen">
+    <div className="flex items-center">
       <div className="mx-auto justify-center">
         <div className="w-[350px] h-[500px] bg-white shadow-lg rounded-lg overflow-hidden">
           <div className="w-full h-56 bg-cover bg-center">
             <div className="w-full px-4 py-4 bg-white">
               <div className="flex items-center justify-left mb-3">
-                <h3 className="text-gray-600 text-2xl mt-1 mr-5">Date: </h3>
-                <DatePicker
-                  className="text-gray-600 text-2xl mt-1"
-                  selected={startDate}
-                  onChange={(date) => setStartDate(date)}
-                />
+                <h3 className="text-gray-600 text-2xl mt-1 mr-5">
+                  Date: {card.date}
+                </h3>
               </div>
               {/* <div className=" items-center">
                 <div className="flex flex-col align-middle justify-between">
--- client/src/Components/CardDisplay.jsx
+++ client/src/Components/CardDisplay.jsx
@@ -2,26 +2,14 @@ import React, { useState, useEffect } from "react";
 import Card from "./Card";
 
 const CardDisplay = ({ user }) => {
-  console.log(user);
-
-  /*  useEffect(() => {
-    const getCards = async () => {
-      try {
-        const response = await fetch(
-          `http://localhost:3000/api/user/${user.username}/cards`
-        );
-        const data = await response.json();
-        console.log(data);
-        setCards(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    getCards();
-  }, []); */
+  const displayCards = user.userCards.map((card) => {
+    return <Card key={card._id} card={card} />;
+  });
 
   return (
     <div className="text-center align-middle mt-10">
+      <div className="flex flex-row justify-center my-10">{displayCards}</div>
+
       <button className="bg-[#7e6e45] rounded-xl text-white px-3 py-1">
         <p>Add Card</p>
       </button>
