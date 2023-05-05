4eea6233e7e458caa753a4873f92f7f233d38653
+ <andrewjohnjulian@gmail.com>
2023-05-04 22:46:33 -0600

    add form modal created

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -18,9 +18,17 @@ const sampleTasks = [
 ];
 
 const Card = ({ card }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [newTask, setNewTask] = useState("");
+
   const displayTasks = sampleTasks.map((task, index) => {
     return <CardTasks key={index} task={task} />;
   });
+
+  const addNewTask = () => {
+    console.log("add task");
+    setShowModal(false);
+  };
 
   return (
     <div className="flex items-center">
@@ -41,9 +49,58 @@ const Card = ({ card }) => {
                   {displayTasks}
                 </div>
               </div>
-              <button className="bg-[#7e6e45] rounded-xl text-white my-3 px-3 py-1">
+              <button
+                type="button"
+                onClick={() => setShowModal(true)}
+                className="bg-[#7e6e45] rounded-xl text-white my-3 px-3 py-1"
+              >
                 Add Task
               </button>
+              {showModal ? (
+                <>
+                  <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                    <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                      {/*content*/}
+                      <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                        {/*header*/}
+                        <div className="flex items-start justify-center p-5 border-b border-solid border-slate-200 rounded-t">
+                          <h3 className="text-3xl font-semibold ">Add Task</h3>
+                        </div>
+                        {/*body*/}
+                        <form
+                          onSubmit={addNewTask}
+                          className="relative p-6 flex-auto"
+                        >
+                          <input
+                            type="text"
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            placeholder="Add new task..."
+                            value={newTask}
+                            onChange={(e) => setNewTask(e.target.value)}
+                          />
+
+                          <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                            <button
+                              className="text-black hover:text-red-600 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                              type="button"
+                              onClick={() => setShowModal(false)}
+                            >
+                              Close
+                            </button>
+                            <button
+                              type="submit"
+                              className="bg-[#7e6e45] rounded-xl text-white px-3 py-1  active:bg-[#7e6e45] font-bold uppercase text-sm  shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                            >
+                              Save Changes
+                            </button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                </>
+              ) : null}
             </div>
           </div>
         </div>
